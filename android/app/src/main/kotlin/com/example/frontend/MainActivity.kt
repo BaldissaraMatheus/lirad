f3a64355c3e8394e6527cd9b89a6dbc2091f9ea6
unknown <baldissaramatheus@gmail.com>
2020-10-31 17:09:44 -0300

    Inclui suporte para notificacoes

--- android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.frontend
+package com.lirad.liradapp
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import './config/palette.dart';
 import 'router_generator.dart';
@@ -22,7 +23,27 @@ final theme = ThemeData(
   ),
 );
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  // https://stackoverflow.com/questions/57689492/flutter-unhandled-exception-servicesbinding-defaultbinarymessenger-was-accesse
+  WidgetsFlutterBinding.ensureInitialized();
+  FirebaseMessaging fm = FirebaseMessaging();
+  var token = await fm.getToken();
+  print(token);
+  fm.configure(
+    onMessage: (Map<String, dynamic> message) async {
+      print("onMessage: $message");
+      // _showItemDialog(message);
+    },
+    // onBackgroundMessage: myBackgroundMessageHandler,
+    onLaunch: (Map<String, dynamic> message) async {
+      print("onLaunch: $message");
+      // _navigateToItemDetail(message);
+    },
+    onResume: (Map<String, dynamic> message) async {
+      print("onResume: $message");
+      // _navigateToItemDetail(message);
+    },
+  );
   runApp(LiradApp());
 }
 
