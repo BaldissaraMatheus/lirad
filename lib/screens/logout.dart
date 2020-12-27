df54f72e6d3633f9aeebf1f26b3464abdd426af8
unknown <baldissaramatheus@gmail.com>
2020-12-27 13:14:29 -0300

    feat: centraliza conteudo do menu de login

--- lib/screens/logout.dart
+++ lib/screens/logout.dart
@@ -5,46 +5,41 @@ class LogoutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        child: Scaffold(
-          backgroundColor: Theme.of(context).backgroundColor,
-          body: SingleChildScrollView(
-            padding: EdgeInsets.fromLTRB(0, 160, 0, 0),
-            child: Align(
-              alignment: Alignment.center,
-              child: Column(
-                children: <Widget>[
-                  Text('Tem certeza que deseja sair?', style: TextStyle(fontSize: 18),),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(100, 20, 100, 0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(children: [
-                          RaisedButton(
-                            child: Text('Confirmar'),
-                            color: Theme.of(context).primaryColor,
-                            textColor: Theme.of(context).primaryTextTheme.bodyText1.color,
-                            onPressed: () => authService.signOut(context)
-                          ),
-                        ]),
-                        Column(children: [
-                          RaisedButton(
-                            child: Text('Cancelar'),
-                            color: Theme.of(context).primaryColor,
-                            textColor: Theme.of(context).primaryTextTheme.bodyText1.color,
-                            onPressed: () => Navigator.of(context).popAndPushNamed('/')
-                          ),
-                        ]),
-                      ]
-                    )
-                  )
-                ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Tem certeza que deseja sair?', style: TextStyle(fontSize: 18),),
+              Container(
+                padding: EdgeInsets.fromLTRB(100, 20, 100, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(children: [
+                      RaisedButton(
+                        child: Text('Confirmar'),
+                        color: Theme.of(context).primaryColor,
+                        textColor: Theme.of(context).primaryTextTheme.bodyText1.color,
+                        onPressed: () => authService.signOut(context)
+                      ),
+                    ]),
+                    Column(children: [
+                      RaisedButton(
+                        child: Text('Cancelar'),
+                        color: Theme.of(context).primaryColor,
+                        textColor: Theme.of(context).primaryTextTheme.bodyText1.color,
+                        onPressed: () => Navigator.of(context).popAndPushNamed('/')
+                      ),
+                    ]),
+                  ]
+                )
               )
-            ),
-          ),
+            ],
+          )
         ),
       ),
     );
